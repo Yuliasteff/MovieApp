50f0c1019b72a619e2966c05d7775a929145d5cc
Julia <darovaniey@tut.by>
2021-04-21 16:57:37 +0300

    new commit

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -1,11 +1,35 @@
-package com.example.movieapp
+package com.example.movies
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.terrakok.cicerone.Navigator
+import com.github.terrakok.cicerone.androidx.AppNavigator
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(R.layout.activity_main){
+    private val navigator: Navigator = AppNavigator(this, R.id.container)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //test
+
+
+
+
+
+        if (savedInstanceState == null) {
+            router.replaceScreen(Screens.MovieList())
+        }
     }
+
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        navigatorHolder.setNavigator(navigator)
+    }
+
+    override fun onPause(){
+        navigatorHolder.removeNavigator()
+        super.onPause()
+    }
+
 }
